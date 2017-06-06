baeb4a7f3169940e80b54208fe2141bafd54fbe7
beatle28c <nautilus28c@gmail.com>
2017-06-06 08:46:45 -0600

    No se que toque en estos arquivos

--- install_Pruebav1.sh
+++ install_Pruebav1.sh
@@ -6,11 +6,18 @@ apt-get install python-matplotlib
 pip install pyserial
 apt-get install python python-tk idle python-pmw python-imaging
 git clone https://github.com/javiercr28/SerialKepco
-mv /home/SerialKepco/button.py /usr/local/lib/python2.7/dist-packages
-mv /home/SerialKepco/graphics.py /usr/local/lib/python2.7/dist-packages
-python /home/SerialKepco/setup.py install
-python /home/SerialKepco/setup2.py install
 pip install adafruit-ads1x15
 apt-get install -y python-smbus
 apt-get install -y i2c-tools
 apt-get install git build-essential python-dev
+
+mv /home/SerialKepco/button.py /usr/local/lib/python2.7/dist-packages
+mv /home/SerialKepco/graphics.py /usr/local/lib/python2.7/dist-packages
+mv /home/SerialKepco/graphics.py /usr/local/lib/python2.7/dist-packages
+mv /home/SerialKepco/graphics.py /usr/local/lib/python2.7/dist-packages
+mv /home/SerialKepco/graphics.py /usr/local/lib/python2.7/dist-packages
+
+python /home/SerialKepco/setup.py install
+python /home/SerialKepco/setup2.py install
+
+
